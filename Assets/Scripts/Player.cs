a3762402e2245fc4642f925940268e517f83f3c3
Dante <dlc241@txstate.edu>
2021-01-03 15:12:28 -0600

    Implemeented netowrking script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class Player : Mirror.NetworkBehaviour
+{
+    [Range(0.1f,10f)]
+    public float speed;
+    void HandleMovement() 
+    {
+        if(isLocalPlayer)
+        {
+            float moveHoriz = Input.GetAxis("Horizontal");
+            float moveVert = Input.GetAxis("Vertical");
+            Vector3 movement = new Vector3(moveHoriz, 0, moveVert) * speed;
+            transform.position = transform.position + movement;
+        }
+    }
+
+    void Update() 
+    {
+        HandleMovement();
+    }
+}
